4ed0a0afa481dff74c2be1e78c5c22c4e4ccfa72
durgadharshini532-ops <durgadharshini532@gmail.com>
2026-04-19 14:15:48 +0530

    Update upcounter_tb.v

--- upcounter_tb.v
+++ upcounter_tb.v
@@ -1,1 +1,20 @@
 
+module tb;
+  reg clk,rst;
+  wire[3:0]out;
+  counter all(clk,rst,out);
+    initial clk=0;
+    always#1clk=~clk;
+  initial begin
+    rst=1;
+    #1 rst=0;
+    #30$finish;
+  end
+initial begin
+  $monitor("clk=%b,rst=%b,out=%b",clk,rst,out);
+  $dumpfile("wave.vcd");
+  $dumpvars();
+end
+endmodule
+    
+    
